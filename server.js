9ad3c6ccc5d19b9a6447139f2099f85408567dbb
Danilo Barbieri <danilobarbieri030@gmail.com>
2024-08-09 21:54:27 -0300

    feat: criação da requisição HTTP com API

---
+++ server.js
@@ -0,0 +1,26 @@
+const express = require('express');
+const axios = require('axios');
+
+const app = express();
+const port = 3000;
+
+app.get('/', (req, res) => {
+  res.send('Hello World!');
+});
+
+app.get('/consulta-cep/:cep', async (req, res) => {
+    const cep = req.params.cep;
+
+    try {
+        const response = await axios.get(`https://viacep.com.br/ws/${cep}/json/`);
+        const data = response.data;
+        res.json(data);
+    } catch (error) {
+        console.error(error);
+        res.status(500).json({ error: 'Erro ao consultar o CEP' });
+    }
+});
+
+app.listen(port, () => {
+  console.log(`Sevidor rodando em http://localhost:${port}`);
+});
